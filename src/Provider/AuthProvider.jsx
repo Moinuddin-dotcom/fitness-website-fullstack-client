55b18096a7142032d1287775943ff164f804214e
Moinuddin-dotcom <moinchy7@gmail.com>
2025-01-16 17:37:30 +0600

    firebase setup - working on authprovider

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+import { app } from "../Firebase/Firebase.config";
+import { getAuth } from "firebase/auth"
+
+
+
+export const AuthContext = createContext(null)
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+
+
+
+
+
+
+    const authInfo = {
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,18 @@ import { RouterProvider } from 'react-router-dom'
 import { Router } from './Routes/Router.jsx'
 import { Toaster } from 'react-hot-toast'
 import { HelmetProvider } from 'react-helmet-async'
+import AuthProvider from './Provider/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      {/* Query client */}
-      <RouterProvider router={Router} />
-      {/* Query client */}
-      <Toaster position='top-right' reverseOrder={false} />
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        {/* Query client */}
+        <RouterProvider router={Router} />
+        {/* Query client */}
+        <Toaster position='top-right' reverseOrder={false} />
+      </HelmetProvider>
+    </AuthProvider>
+
   </StrictMode>,
 )
